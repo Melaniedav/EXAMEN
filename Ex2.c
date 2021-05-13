e507b558dcd6e96215d1eeda407b380616b63272
Melaniedav <82843300+Melaniedav@users.noreply.github.com>
2021-05-12 22:00:20 -0500

    Add files via upload

--- Ex2.c
+++ Ex2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main ()
 {
-    printf("Melanie Dayana Aguilar Vergara")
+    printf("Melanie Dayana Aguilar Vergara");
     float a, b, c;
     printf ("\n \n \n \nPrograma 2 \n");
     printf ("Suma de numeros \n");
     printf ("\n\nDame el valor entero de a: ");
-    scanf ("%i", &a);
+    scanf ("%f", &a);
     printf ("\n \nDame el valor entero de b: ");
-    scanf ("%i", &b);
+    scanf ("%f", &b);
     c = a + b;
     printf ("\n El resultado es %d",c);
     return 0;
